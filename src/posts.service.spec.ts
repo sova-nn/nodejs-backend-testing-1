774611b64df40aa53914e7fbdc914d7074604d95
Marina <nmo1@ya.ru>
2024-12-15 20:00:37 +0300

    1. Tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -13,10 +13,39 @@ describe('PostsService', () => {
   });
 
   it('should add a new post', () => {
-    // реализуйте тест-кейс
+    const result = postsService.create(post);
+
+    expect(result).toEqual({
+      id: expect.any(String),
+      text: expect.any(String),
+      date: expect.any(String),
+    });
+
+    expect(result).toHaveProperty('text', result.text);
+    expect(result).toHaveProperty('text', expect.any(String));
+
+    expect(result).toMatchObject({
+      id: expect.any(String),
+      text: expect.any(String),
+      date: expect.any(String),
+    });
+    expect(result).not.toEqual(post);
   });
 
   it('should find a post', () => {
-    // реализуйте тест-кейс
+    const createdPost = postsService.create(post);
+    const result = postsService.find(createdPost.id);
+
+    expect(result).toEqual({
+      id: expect.any(String),
+      text: expect.any(String),
+      date: expect.any(String),
+    });
+
+    expect(result).toHaveProperty('id', createdPost.id);
+    expect(result).toMatchObject(createdPost);
+
+    expect(result).not.toEqual(-1);
+    expect(result).not.toEqual(undefined);
   });
 });
